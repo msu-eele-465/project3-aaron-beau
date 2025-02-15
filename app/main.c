47eb471af786449c264bd760c3f2a18ffb125499
Aaron Foster <apfozz223@gmail.com>
2025-02-15 14:04:54 -0700

    Adding heartbeat LED and 1Hz timer (need 0.5Hz timer)

--- app/main.c
+++ app/main.c
@@ -1,16 +1,52 @@
 #include <msp430.h>
-#include <stdbool.h>
+#include "heartbeat.h"
+
+volatile unsigned int overflow_count = 0;   // Overflow counter
 
 int main(void)
 {
-    // Stop watchdog timer
-    WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer
-    
-    // Disable the GPIO power-on default high-impedance mdoe to activate
-    // previously configure port settings
-    PM5CTL0 &= ~LOCKLPM5;
 
-    while(true)
-    {
+    WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer
+
+    P6DIR |= BIT6;                          // Configure LED on P6.6
+    P6OUT &= ~BIT6;
+
+    PM5CTL0 &= ~LOCKLPM5;                   // Disable GPIO high-impedance mode
+
+
+    // Setup Timer B0 (for heartbeat)
+    TB0CTL |= TBCLR;                        // Clear timer
+    TB0CTL |= TBSSEL__SMCLK;                // Select SMCLK (1 MHz)
+    TB0CTL |= MC__CONTINUOUS;               // Set mode to continuous
+    TB0CTL |= ID_3;                         // Divide clock by 8 (1MHz / 8 = 125kHz)
+    TB0CCR0 = 125000;                       // Set overflow to 1 second (125000 / 125kHz = 1s)
+
+    TB0CTL |= TBIE;                         // Enable Timer Overflow Interrupt
+
+    TB0CTL &= ~TBIFG;
+
+    __enable_interrupt();                   //Global
+
+    while (1) {
     }
 }
+
+
+//------------------------------------------------------------------------------
+//----------------------Start Timer Overflow ISR--------------------------------
+//------------------------------------------------------------------------------
+// 1 Second Functionality for several uses
+//------------------------------------------------------------------------------
+#pragma vector = TIMER0_B1_VECTOR
+__interrupt void ISR_TB0_OVERFLOW(void)
+{
+
+        heartbeat_toggle();                  // Call heartbeat function to toggle LED
+ 
+
+    TB0CTL &= ~TBIFG;                        // Clear interrupt flag
+}
+//------------------------------------------------------------------------------
+//------------------------End Timer Overflow ISR--------------------------------
+//------------------------------------------------------------------------------
+
