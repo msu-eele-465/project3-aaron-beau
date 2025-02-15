cfb306384e5c57ad9a0728be85d10e7ab4102b33
bcoburn0618 <beaucoburn2003@gmail.com>
2025-02-15 15:24:53 -0700

    Key Pad Testing Development Project I have added a project to test and develop the keypad to to be implemented into the overall project.

---
+++ test/keypad_test/keypad_test.c
@@ -0,0 +1,48 @@
+/*----------------------------------------------------------------------------------
+Beau Coburn & Aaron Foster
+EELE 465
+2/20/25
+Project 3: Keypad LED Pattersn
+Description:  In the below code, the logic for analyzing which key has been pressed
+on the keypad is defined.
+
+Pin Out
+--------------
+Rows : P1.4-7
+Cols : P6.0-3
+
+
+-------------------------------------------------------------------------------------*/
+#include <msp430.h>
+
+// Keypad mapping
+char keymap[4][4] = {
+    {'1', '2', '3', 'A'},
+    {'4', '5', '6', 'B'},
+    {'7', '8', '9', 'C'},
+    {'*', '0', '#', 'D'}
+};
+
+char scan_keypad(void){
+    int row, col;
+
+}
+
+int main(void)
+{
+    WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer
+    
+    
+
+    P1OUT &= ~BIT0;                         // Clear P1.0 output
+    P1DIR |= BIT0;                          // Set P1.0 to output direction
+
+    PM5CTL0 &= ~LOCKLPM5;                   // Disable the GPIO power-on default high-impedance mode
+                                            
+
+    while(1)
+    {
+        P1OUT ^= BIT0;                      // Toggle P1.0 using exclusive-OR
+        __delay_cycles(100000);             // Delay for 100000*(1/MCLK)=0.1s
+    }
+}
