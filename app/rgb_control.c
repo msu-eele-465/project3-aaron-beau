5f8af057d53f51b6de9ab7639c4e291a02b49c60
Aaron Foster <apfozz223@gmail.com>
2025-02-18 11:36:21 -0700

    fine-tuning heartbeat for merge

--- app/rgb_control.c
+++ app/rgb_control.c
@@ -4,25 +4,25 @@
 void rgb_control(int color){        //1=red, 2=green, 3=blue, 4=yellow
 
     if(color == 1){
-        P4OUT ^= BIT0;
+        P4OUT |= BIT0;
         P2OUT &= ~BIT2;
         P2OUT &= ~BIT0;
 
     }else if (color == 2) {
         P4OUT &= ~BIT0;
-        P2OUT ^= BIT2;
+        P2OUT |= BIT2;
         P2OUT &= ~BIT0;
 
     }else if (color == 3) {
         P4OUT &= ~BIT0;
         P2OUT &= ~BIT2;
-        P2OUT ^= BIT0;
+        P2OUT |= BIT0;
     }else if (color ==4) {
-        P4OUT ^= BIT0;
-        P2OUT ^= BIT2;
+        P4OUT |= BIT0;
+        P2OUT |= BIT2;
         P2OUT &= ~BIT0;
 
-        P4OUT ^= BIT0;
-        P2OUT ^= BIT2;
+        P4OUT |= BIT0;
+        P2OUT |= BIT2;
     }
 }
